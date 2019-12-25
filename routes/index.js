e599189b6110718ea6ad88f69479fc3ca1ebbafe
Alex Kubecka <akubecka@stevens.edu>
2019-12-25 11:28:08 -0500

    ready to start scraping

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,7 @@ const teamRoutes = require("./teams");
 const leagueRoutes = require("./leagues");
 const homeRoute = require("./home");
 const sportRoutes = require("./sport");
-const soccerRoutes = require("./sports/soccer");
-const basketballRoutes = require("./sports/basketball");
-const baseballRoutes = require("./sports/baseball");
-const footballRoutes = require("./sports/football");
+
 
 const constructorMethod = app => {
   app.use("/", homeRoute)
@@ -14,11 +11,6 @@ const constructorMethod = app => {
   app.use("/teams", teamRoutes);
   app.use("/leagues", leagueRoutes);
   app.use("/sport", sportRoutes);
-  app.use("/sports/soccer", soccerRoutes);
-  app.use("/sports/basketball", basketballRoutes);
-  app.use("/sports/baseball", baseballRoutes);
-  app.use("/sports/football", footballRoutes);
-  
 
   app.use("*", (req, res) => {
     res.status(404).json({ error: "Not found" });
--- routes/sport.js
+++ routes/sport.js
@@ -4,12 +4,85 @@ const data = require("../data");
 const playerData = data.players;
 const teamData = data.teams;
 const leagueData = data.leagues;
-
+const fs = require("fs");
   router.post("/sport", async function (req,res){
     try{
         const sporty = req.body.sport;
         const rend = "layouts/sports/" + sporty;
-        res.render(rend, {sport: sporty});
+        if(sporty=='soccer'){
+          res.render("layouts/home", {title: "FM-Online",sport: sporty, soccer: true});
+        }else if(sporty=='football'){
+          res.render("layouts/home", {title: "FM-Online",sport: sporty, football: true});
+        }else if(sporty=='basketball'){
+          res.render("layouts/home", {title: "FM-Online",sport: sporty, basketball: true});
+        }else if(sporty=='baseball'){
+          res.render("layouts/home", {title: "FM-Online",sport: sporty, baseball: true});
+        }
+        return;
+    }catch(e){
+      console.log(e);
+      res.sendStatus(500);
+    }
+  });
+
+  router.post("/soccerLeague", async function (req,res){
+    try{
+        const leag = req.body.league;
+        const rend = "layouts/soccer/"+ leag;
+        console.log("wuh");
+        fs.readFile(__dirname+'/' + leag + '.txt', 'utf8', function(err, contents) {
+          if (err) throw err;
+          console.log(contents);
+          res.render(rend, {league: leag, file: contents});
+        });
+        return;
+    }catch(e){
+      console.log(e);
+      res.sendStatus(500);
+    }
+  });
+
+  router.post("/baseballLeague", async function (req,res){
+    try{
+        const leag = req.body.league;
+        const rend = "layouts/baseball/"+ leag;
+        fs.readFile(__dirname+'/' + leag + '.txt', 'utf8', function(err, contents) {
+          if (err) throw err;
+          console.log(contents);
+          res.render(rend, {league: leag, file: contents});
+        });
+        return;
+    }catch(e){
+      console.log(e);
+      res.sendStatus(500);
+    }
+  });
+
+  router.post("/basketballLeague", async function (req,res){
+    try{
+        const leag = req.body.league;
+        const rend = "layouts/basketball/"+ leag;
+        fs.readFile(__dirname+'/' + leag + '.txt', 'utf8', function(err, contents) {
+          if (err) throw err;
+          console.log(contents);
+          res.render(rend, {league: leag, file: contents});
+        });
+        return;
+    }catch(e){
+      console.log(e);
+      res.sendStatus(500);
+    }
+  });
+
+  router.post("/footballLeague", async function (req,res){
+    try{
+        const leag = req.body.league;
+        const rend = "layouts/football/"+ leag;
+        fs.readFile(__dirname+'/' + leag + '.txt', 'utf8', function(err, contents) {
+          if (err) throw err;
+          console.log(contents);
+          res.render(rend, {league: leag, file: contents});
+        });
         return;
     }catch(e){
       console.log(e);
